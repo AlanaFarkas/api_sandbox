7e1c3f1833db4167b0acb86e8993169a88b28e14
Alana Farkas <alana.farkas@gmail.com>
2017-07-23 19:20:01 -0400

    working input button and select dropdown

--- weatherapi.js
+++ weatherapi.js
@@ -1,20 +1,8 @@
 $(document).ready(function(){
 
-	$("#submit-button, #submit-button-select").click(function(e){
+	$("#submit-button").click(function(e){
 		e.preventDefault();
 		var zip = $("#zip-input").val();
-		console.log(zip);
-
-
-		// var select = $("#zip-select option:selected").val()
-		// console.log(select);
-
-		var str = "";
-  		$("select option:selected").each(function () {
-        	str += $(this).text() + " ";
-        	console.log(str);
-      	});
-
 
 		$.ajax({
 			url: "http://api.wunderground.com/api/09f8aa654d3c3800/conditions/q/" + zip +".json",
@@ -27,8 +15,35 @@ $(document).ready(function(){
 
 				$("body").append("<div class='zip'>" + zip + "</div>");
 		   	}
+		});	//.ajax
+
+	 }); //.click
+
+	$("#submit-button-select").click(function(e){
+		e.preventDefault();
+		var zip = $("#zip-select").val();
+		
+		// var select = $("#zip-select option:selected").val()
+		// console.log(select);
+
+		var str = "";
+  		$("select option:selected").each(function () {
+        	str += $(this).text() + " ";
+        	console.log(str);
+      	});
 
 
+		$.ajax({
+			url: "http://api.wunderground.com/api/09f8aa654d3c3800/conditions/q/" + str +".json",
+			dataType: 'json',
+			method: 'get',
+			cache: true,
+				success: function(data){
+				var zip = data.current_observation.observation_location.full;
+				console.log(zip);
+
+				$("body").append("<div class='zip'>" + zip + "</div>");
+		   	}
 		});	//.ajax
 
 	 }); //.click
